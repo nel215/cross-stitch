c1f82e09277d9bbb52e9aa3fb625394af27b9e6f
nel215 <otomo.yuhei@gmail.com>
2017-03-08 21:49:27 +0900

    Refactor unused code

--- CrossStitch.hpp
+++ CrossStitch.hpp
@@ -38,52 +38,6 @@ string to_string(int r, int c){
     return oss.str();
 }
 
-
-int xor128() {
-  static int x = 123456789;
-  static int y = 362436069;
-  static int z = 521288629;
-  static int w = 88675123;
-
-  int t = x ^ (x << 11);
-  x = y; y = z; z = w;
-  return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-}
-
-typedef vector<vector<int> > NNGraph;
-
-class State{
-    void init(int pos, int score, int hash, State *prev){
-        this->pos = pos;
-        this->score = score;
-        this->hash = hash;
-        this->prev = prev;
-    }
-public:
-    int pos;
-    int score;
-    State *prev;
-    int hash;
-    State(int pos){
-        this->init(pos, 0, 0, NULL);
-    }
-    State(int pos, int score, int hash, State *prev){
-        this->init(pos, score, hash, prev);
-    }
-    void debug(){
-        cerr << "pos: " << pos << ", score: " << score << endl;
-    }
-};
-
-class StateComparator{
-public:
-    bool operator()(const State *a, const State *b){
-        return a->score > b->score;
-    }
-};
-
-typedef priority_queue<State*, vector<State*>, StateComparator> StateQueue;
-
 struct P{
     int y, x;
     void init(int y, int x){
@@ -111,6 +65,18 @@ struct Stitch{
         init(f, t);
     }
 };
+
+vector<char> create_alphabet(const vector<string> &pattern){
+    vector<char> res;
+    int ps = pattern.size();
+    REP(y, ps)REP(x, ps){
+        if(pattern[y][x]=='.')continue;
+        res.push_back(pattern[y][x]);
+    }
+    sort(ALL(res));
+    res.erase(unique(ALL(res)), res.end());
+    return res;
+}
 
 vector<Stitch> extract_stitches(const vector<string> &pattern, const char c){
     int ps = pattern.size();
@@ -205,208 +171,15 @@ vector<pair<int, int> > search_min_permutation(vector<Stitch> &stitches){
     return vector<pair<int, int> >(ALL(res));
 }
 
-
-
-vector<int> create_hash_table(int s){
-    vector<int> res(s*s);
-    REP(i, res.size())res[i] = xor128();
-    return res;
-}
-
 class CrossStitch {
-protected:
-    vector<string> pattern;
-    vector<char> alphabet;
-    vector<int> hash_table;
-    int pattern_size;
-    void set_pattern(vector<string> pattern){
-        this->pattern = pattern;
-        this->pattern_size = pattern.size();
-        this->alphabet = vector<char>();
-        int ps = this->pattern_size;
-        REP(y, ps)REP(x, ps){
-            if(pattern[y][x]=='.')continue;
-            alphabet.push_back(pattern[y][x]);
-        }
-        sort(ALL(alphabet));
-        alphabet.erase(unique(ALL(alphabet)), alphabet.end());
-        hash_table = create_hash_table(ps);
-    }
-    bool is_valid_position(int y, int x){
-        int ps = this->pattern_size;
-        return y >= 0 && x >=0 && y < ps && x < ps;
-    }
-    int sq_dist_of_pos(int ap, int bp){
-        int ps = this->pattern_size;
-        int ay = ap / ps;
-        int ax = ap % ps;
-        int by = bp / ps;
-        int bx = bp % ps;
-        return sq_dist(ay, ax, by, bx);
-    }
-    vector<int> extract_cells_with_same_color(char c){
-        int ps = this->pattern_size;
-        vector<int> cells;
-
-        REP(y, ps)REP(x, ps){
-            if(this->pattern[y][x]!=c)continue;
-            cells.push_back(y*ps+x);
-        }
-
-        return cells;
-    }
-    NNGraph create_nngraph_with_same_color(char c){
-        int ps = this->pattern_size;
-
-        NNGraph nngraph(ps*ps, vector<int>());
-        REP(y, ps)REP(x, ps){
-            if(this->pattern[y][x]!=c)continue;
-            int from = y*ps + x;
-            vector<pair<int, int> > cand;
-            REP(i, 3)REP(j, 3){
-                int ty = y - 1 + i;
-                int tx = x - 1 + j;
-                if(ty==y && tx==x)continue;
-                if(!is_valid_position(ty, tx))continue;
-                if(pattern[ty][tx]!=c)continue;
-                int sd = sq_dist(y, x, ty, tx);
-
-                int to = ty*ps + tx;
-                cand.push_back(make_pair(sd, to));
-            }
-            if(cand.size()<4){ // TODO: parameterize?
-                cand.clear();
-                REP(ty, ps)REP(tx, ps){
-                    if(ty==y && tx==x)continue;
-                    if(pattern[ty][tx]!=c)continue;
-                    int sd = sq_dist(y, x, ty, tx);
-                    int to = ty*ps + tx;
-                    cand.push_back(make_pair(sd, to));
-                }
-            }
-            sort(ALL(cand));
-            REP(i, min(4, (int)cand.size())){
-                int to = cand[i].second;
-                nngraph[from].push_back(to);
-                nngraph[to].push_back(from);
-            }
-        }
-
-        // unique
-        REP(i, nngraph.size()){
-            sort(ALL(nngraph[i]));
-            nngraph[i].erase(unique(ALL(nngraph[i])), nngraph[i].end());
-        }
-        return nngraph;
-    }
-    vector<State*> extract_next_states(State *current_state, const NNGraph &g, const vector<int> &cells){
-        const State *cs = current_state;
-        int ps = this->pattern_size;
-        vector<int> used(ps*ps);
-
-        // recover
-        State *ptr = current_state;
-        while(ptr!=NULL){
-            int pos = ptr->pos;
-            used[pos] = 1;
-            ptr = ptr->prev;
-        }
-
-        // search
-        vector<State*> next_states;
-        REP(i, g[current_state->pos].size()){
-            int to = g[current_state->pos][i];
-            if(used[to])continue;
-            int score = cs->score + this->sq_dist_of_pos(current_state->pos, to);
-            int hash = cs->hash ^ hash_table[to];
-            State *next_state = new State(to, score, hash, current_state);
-            next_states.push_back(next_state);
-        }
-        if(next_states.size()>0)return next_states;
-
-        REP(i, cells.size()){
-            int to = cells[i];
-            if(used[to])continue;
-            int score = current_state->score + this->sq_dist_of_pos(current_state->pos, to);
-            int hash = cs->hash ^ hash_table[to];
-            State *next_state = new State(to, score, hash, current_state);
-            next_states.push_back(next_state);
-        }
-
-        return next_states;
-    }
-    vector<State*> create_initial_states(const NNGraph &g){
-        vector<pair<int, int> > cand;
-        REP(i, g.size()){
-            if(g[i].size()==0)continue;
-            cand.push_back(make_pair(g[i].size(), i));
-        }
-        sort(ALL(cand));
-        vector<State*> res;
-        REP(i, cand.size()){
-            int p = cand[i].second;
-            res.push_back(new State(p));
-        }
-        return res;
-    }
-    vector<int> search_min_path(vector<State*> initial_states, double limit, const NNGraph &g, const vector<int> &cells){
-        int node_size = 0;
-        REP(i, g.size())if(g[i].size()>0)node_size++;
-
-        vector<StateQueue> beam(node_size, StateQueue());
-        vector<map<int, int> > hash_to_score(node_size, map<int, int>());
-        int init_idx = 0;
-        while(get_time() < limit){
-            if(init_idx < (int)initial_states.size()){
-                State *is = initial_states[init_idx++];
-                beam[0].push(is);
-                hash_to_score[0][is->hash] = is->score;
-            }
-            int b=0;
-            for(;;){
-                if(b==node_size-1)break;
-                if(beam[b].empty())break;
-                State *s = beam[b].top();
-                beam[b].pop();
-                //cerr << "b: " << b << ", ";
-                //s->debug();
-                if(hash_to_score[b][s->hash] != s->score){
-                    continue;
-                }
-                vector<State*> next_states = this->extract_next_states(s, g, cells);
-                if(next_states.size()==0)continue;
-                REP(j, next_states.size()){
-                    State *ns = next_states[j];
-                    if(hash_to_score[b+1].count(ns->hash) > 0 && hash_to_score[b+1][ns->hash] <= ns->score){
-                        continue;
-                    }
-                    hash_to_score[b+1][ns->hash] = ns->score;
-                    beam[b+1].push(ns);
-                }
-                b++;
-            }
-        }
-        //cerr << node_size << beam[node_size-1].size() << endl;
-
-        State *ptr = beam[node_size-1].top();
-        vector<int> res;
-        while(ptr!=NULL){
-            res.push_back(ptr->pos);
-            ptr = ptr->prev;
-        }
-        reverse(ALL(res));
-        return res;
-    }
 public:
-
     vector<string> embroider(vector<string> pattern) {
         double start_time = get_time();
-        this->set_pattern(pattern);
         vector<string> ret;
-        int ps = this->pattern_size;
-        // for each color, for each cell (r, c) do two stitches (r+1, c)-(r, c+1)-(r+1, c+1)-(r, c)
+
+        vector<char> alphabet = create_alphabet(pattern);
         double each_time = 8.0 / alphabet.size();
-        INFO(<< "S=" << this->pattern_size);
+        INFO(<< "S=" << pattern.size());
         INFO(<< "C=" << alphabet.size());
         REP(i, alphabet.size()){
             char c = alphabet[i];
@@ -422,31 +195,7 @@ public:
                 ret.push_back(to_string(f.y, f.x));
                 ret.push_back(to_string(t.y, t.x));
             }
-            //NNGraph g = create_nngraph_with_same_color(c);
-            //vector<int> cells = extract_cells_with_same_color(c);
-            //vector<State*> initial_states = create_initial_states(g);
-            //double limit = start_time + each_time*(i+1);
-            //vector<int> min_path = search_min_path(initial_states, limit, g, cells);
-            //ret.push_back(string(1, c));
-            //REP(j, min_path.size()){
-            //    int p = min_path[j];
-            //    int y = p / ps;
-            //    int x = p % ps;
-            //    int last = (int)ret.size()-1;
-            //    if(ret[last]!=to_string(y+1, x)){
-            //        ret.push_back(to_string(y+1, x));
-            //        ret.push_back(to_string(y, x+1));
-            //        ret.push_back(to_string(y+1, x+1));
-            //        ret.push_back(to_string(y, x));
-            //    }else{
-            //        ret.push_back(to_string(y+1, x+1));
-            //        ret.push_back(to_string(y, x));
-            //        ret.push_back(to_string(y+1, x));
-            //        ret.push_back(to_string(y, x+1));
-            //    }
-            //}
         }
         return ret;
     }
 };
-
--- test.cpp
+++ test.cpp
@@ -15,87 +15,8 @@ void test_extract_stitches(){
     assert(min_perm.size()==stitches.size());
 }
 
-
-class TestingCrossStitch : public CrossStitch {
-public:
-    void test_extract_cells_with_same_color(){
-        vector<string> p;
-        p.push_back(".a");
-        p.push_back("..");
-        this->set_pattern(p);
-
-        vector<int> cells = extract_cells_with_same_color('a');
-        assert(cells.size() == 1);
-        assert(cells[0] == 1);
-    }
-    void test_create_nngraph_with_same_color(){
-        vector<string> p;
-        p.push_back(".a..");
-        p.push_back("aaa.");
-        p.push_back(".a..");
-        p.push_back("...a");
-        this->set_pattern(p);
-
-        NNGraph nngraph = this->create_nngraph_with_same_color('a');
-        assert(nngraph.size() == 16);
-        assert(nngraph[5].size() == 5);
-        assert(nngraph[15].size() == 4);
-    }
-    void test_extract_next_states(){
-        vector<string> p;
-        p.push_back("aa");
-        p.push_back("..");
-        this->set_pattern(p);
-
-        NNGraph g(4);
-        g[0].push_back(1);
-        g[1].push_back(2);
-        State s1(0);
-        State s2(1, 1, 0, &s1);
-        vector<State*> next_states = this->extract_next_states(&s2, g, vector<int>());
-        assert(next_states.size() == 1);
-        assert(next_states[0]->pos == 2);
-        assert(next_states[0]->hash != 0);
-        assert(next_states[0]->score == 3);
-    }
-
-    void test_create_initial_states(){
-        NNGraph g(4);
-        g[0].push_back(1);
-        g[0].push_back(2);
-        g[1].push_back(2);
-
-        vector<State*> initial_states = this->create_initial_states(g);
-        assert(initial_states.size() == 2);
-        assert(initial_states[0]->pos == 1);
-        assert(initial_states[0]->score == 0);
-    }
-
-    void test_search_min_path(){
-        double start_time = get_time();
-        vector<string> p;
-        p.push_back(".a..");
-        p.push_back("aaa.");
-        p.push_back(".a..");
-        p.push_back("...a");
-        this->set_pattern(p);
-
-        NNGraph g = this->create_nngraph_with_same_color('a');
-        vector<State*> initial_states;
-        initial_states.push_back(new State(15));
-        initial_states.push_back(new State(1));
-        vector<int> min_path = this->search_min_path(initial_states, start_time+1.0, g, vector<int>());
-        assert(min_path.size()==6);
-    }
-};
-
 int main(){
-    TestingCrossStitch tcs;
-    tcs.test_extract_cells_with_same_color();
-    tcs.test_create_nngraph_with_same_color();
-    tcs.test_extract_next_states();
-    tcs.test_create_initial_states();
-    tcs.test_search_min_path();
+    test_extract_stitches();
     cout << "all tests succeeded." << endl;
     return 0;
 }
